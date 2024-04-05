b6dcec477f0c2b788d1e970527824b9c54faad0a
Daavvvvvv <davidvelez006@gmail.com>
2024-04-04 22:02:06 -0500

    Completly added navItems with the correct functionality

--- app/(platform)/(dashboard)/_components/nav-item.tsx
+++ app/(platform)/(dashboard)/_components/nav-item.tsx
@@ -1,25 +1,51 @@
-import { Navbar } from '../../../(marketing)/_components/navbar';
 "use client";
+
+import { AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
 
 
 export type Organization = {
-    id: string;
-    slug: string;
-    imageUrl: string;
-    name: string;
-}
+  id: string;
+  slug: string;
+  imageUrl: string;
+  name: string;
+};
 
 interface NavItemProps {
-    isExpanded: boolean;
-    isActive: boolean;
-    organization: any;
-    onExpand: (id:string) => void
+  isExpanded: boolean;
+  isActive: boolean;
+  organization: any;
+  onExpand: (id: string) => void;
 }
 
-export const NavItem = ({isExpanded, isActive, organization, onExpand}:NavItemProps) => {
-    return(
-        <div>
-            NavItem
+export const NavItem = ({
+  isExpanded,
+  isActive,
+  organization,
+  onExpand,
+}: NavItemProps) => {
+  return (
+    <AccordionItem
+      value={organization.id}
+      className="flex items-center justify-between w-full">
+      <AccordionTrigger
+        onClick={() => onExpand(organization.id)}
+        className={cn(
+          "flex items-center gap-x-2 p-1.5 text-neutral-700 rounded-md hover:bg-neutral-500/10 transition text-start no-underline hover:no-underline w-full",
+          isActive && !isExpanded && "bg-sky-500/10 text-sky-700"
+        )}>
+        <div className="flex items-center gap-x-2">
+          <div className="w-7 h-7 relative">
+            <Image
+              fill
+              src={organization.imageUrl}
+              alt={organization.name}
+              className="rounded-full object-cover"
+            />
+          </div>
         </div>
-    )
-}
+      </AccordionTrigger>
+    </AccordionItem>
+  );
+};
